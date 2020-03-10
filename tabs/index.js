4b534df4b83db89853d9d1b9dce64cf93d8f6607
alex <awebdeff@gmail.com>
2020-03-10 14:37:21 +0300

    done

--- tabs/index.js
+++ tabs/index.js
@@ -1,16 +1,34 @@
+(() => {
+
 let tab = function () {
-  const tabNav = document.querySelectorAll('.tabs-nav__item')
-  const tabContent = document.querySelectorAll('.tab')
-  const tabName
+
+  let tabNav = document.querySelectorAll('.tabs-nav__item')
+  let tabContent = document.querySelectorAll('.tab')
+  let tabName
 
   tabNav.forEach(item => {
     item.addEventListener('click', selectTabNav)
   })
 
   function selectTabNav() {
-    console.log(this)
+    tabNav.forEach(item => {
+      item.classList.remove('is-active')
+    })
+    this.classList.add('is-active')
+    tabName = this.getAttribute('data-tab-name')
+    selectTabContent(tabName)
   }
+
+  function selectTabContent(tabName) {
+    tabContent.forEach(item => {
+      item.classList.contains(tabName) ?
+        item.classList.add('is-active') :
+        item.classList.remove('is-active')
+    })
+  }
+
 }
 
 tab()
 
+})()
